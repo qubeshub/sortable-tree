2a9ff53807d5a9f67584f222a3f02098044685cc
M. Drew LaMar <mdlama@wm.edu>
2022-08-22 15:59:11 -0400

    Fixed source map warning

--- webpack.conf.js
+++ webpack.conf.js
@@ -23,6 +23,11 @@ module.exports = {
                 }
             },
             {
+                test: /\.js$/,
+                enforce: 'pre',
+                use: ['source-map-loader'],
+            },
+            {
                 test: /\.css$/,
                 use: [
                     "css-loader"
